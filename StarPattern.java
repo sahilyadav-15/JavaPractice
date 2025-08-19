25799b08a980833918ddc6bbd4a3000392ec78e3
sahilyadav-15 <s6906433@gmail.com>
2025-08-19 18:02:42 +0530

    Initial

---
+++ StarPattern.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class StarPattern {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(n);
+  }
+}
